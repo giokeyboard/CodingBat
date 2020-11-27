57a712b2d7d4f9b8902517810827b01090aae52b
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-11-27 17:33:33 +0000

    Implemented Python Logic-1

---
+++ Python/Logic-1/sorta_sum.py
@@ -0,0 +1,15 @@
+def sorta_sum(a, b):
+    """
+    Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden,
+    so in that case just return 20.
+
+    sorta_sum(3, 4) → 7
+    sorta_sum(9, 4) → 20
+    sorta_sum(10, 11) → 21
+    """
+    return 20 if 10 <= a + b <= 19 else a + b
+
+
+print(sorta_sum(3, 4))
+print(sorta_sum(9, 4))
+print(sorta_sum(10, 11))
