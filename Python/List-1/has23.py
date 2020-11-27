bce59840410d5d98c55111aabc71c095e03f036d
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-11-27 09:52:52 +0000

    Implemented Python List-1

---
+++ Python/List-1/has23.py
@@ -0,0 +1,15 @@
+def has23(nums):
+    """
+    Given an int array length 2, return True if it contains a 2 or a 3.
+
+
+    has23([2, 5]) → True
+    has23([4, 3]) → True
+    has23([4, 5]) → False
+    """
+    return 2 in nums or 3 in nums
+
+
+print(has23([2, 5]))
+print(has23([4, 3]))
+print(has23([4, 5]))
